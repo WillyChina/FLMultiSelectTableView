250400d602aced7d975455a2d7aada45322bd0a3
liweiwei <weiwei.li@ashermed.com>
2022-09-27 09:58:41 +0800

    初始化

---
+++ Example/Pods/Target Support Files/FLMultiSelectTableView/FLMultiSelectTableView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FLMultiSelectTableView : NSObject
+@end
+@implementation PodsDummy_FLMultiSelectTableView
+@end
---
+++ FLMultiSelectTableView/Classes/TestViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestViewController.h
+//  Pods
+//
+//  Created by liweiwei on 2022/9/27.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
